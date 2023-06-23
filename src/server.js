e89fb285f7f24a36869d75b536a3ac9d390badb9
OmarHamada1996 <omer.jasser@gmail.com>
2023-06-23 23:06:43 +0000

    deploying v2

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const booksRouter = require('./routes/books');
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/books', booksRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
